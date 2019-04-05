70b20d282142ebceb22851369dd5ee9df77c6040
Sawyer McLane <samclane@gmail.com>
2019-04-05 11:30:50 -0600

    using tmp directory

--- hello/views.py
+++ hello/views.py
@@ -12,11 +12,13 @@ def index(request):
         phrase = request.POST.get('textfield', None)
         try:
             tts = gTTS(phrase, lang='en')
-            with open(os.path.join(BASE_DIR, r"audio/file.mp3"), 'wb') as f:
+            with open(os.path.join('/tmp/', r"audio/file.mp3"), 'wb') as f:
+                print("~~~POST: ", os.path.join(BASE_DIR, r"audio/file.mp3"))
                 tts.write_to_fp(f)
-            return render(request, "index.html", {"link": os.path.join(BASE_DIR, r"audio/file.mp3")})
+            return render(request, "index.html", {"link": os.path.join('/tmp/', r"audio/file.mp3")})
         except Exception as exc:
             return HttpResponse(f"Exception thrown during speak attempt: {exc}\n")
     else:
+        print("~~~GET: ", os.path.join(BASE_DIR, r"audio/file.mp3"))
         return render(request, "index.html")
 
