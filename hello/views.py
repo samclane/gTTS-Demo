47089b028aeb991dce8d3265059f8ce595446a77
Sawyer McLane <samclane@gmail.com>
2019-04-05 11:32:36 -0600

    removed additional folder depth

--- hello/views.py
+++ hello/views.py
@@ -12,13 +12,11 @@ def index(request):
         phrase = request.POST.get('textfield', None)
         try:
             tts = gTTS(phrase, lang='en')
-            with open(os.path.join('/tmp/', r"audio/file.mp3"), 'wb') as f:
-                print("~~~POST: ", os.path.join(BASE_DIR, r"audio/file.mp3"))
+            with open(os.path.join('/tmp/', r"file.mp3"), 'wb') as f:
                 tts.write_to_fp(f)
-            return render(request, "index.html", {"link": os.path.join('/tmp/', r"audio/file.mp3")})
+            return render(request, "index.html", {"link": os.path.join('/tmp/', r"file.mp3")})
         except Exception as exc:
             return HttpResponse(f"Exception thrown during speak attempt: {exc}\n")
     else:
-        print("~~~GET: ", os.path.join(BASE_DIR, r"audio/file.mp3"))
         return render(request, "index.html")
 
