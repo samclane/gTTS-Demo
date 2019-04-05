f41afdd39b8e5fe102b6c123010e9d028938f9ab
Sawyer McLane <samclane@gmail.com>
2019-04-05 11:53:36 -0600

    added unique file ids

--- hello/views.py
+++ hello/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from gettingstarted.settings import BASE_DIR
 import os
+import uuid
 
 from gtts import gTTS
 
@@ -14,12 +15,13 @@ else:
 # Create your views here.
 def index(request):
     if request.method == 'POST':
+        uid = uuid.uuid4()
         phrase = request.POST.get('textfield', None)
         try:
             tts = gTTS(phrase, lang='en')
-            with open(os.path.join(BASE_DIR, r"file.mp3"), 'wb') as f:
+            with open(os.path.join(BASE_DIR, r"file_{}.mp3".format(uid)), 'wb') as f:
                 tts.write_to_fp(f)
-            return render(request, "index.html", {"link": os.path.join(BASE_DIR, r"file.mp3")})
+            return render(request, "index.html", {"link": os.path.join(BASE_DIR, r"file_{}.mp3".format(uid))})
         except Exception as exc:
             return HttpResponse(f"Exception thrown during speak attempt: {exc}\n")
     else:
