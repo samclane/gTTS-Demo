c91f7c21fec9d0b457d664da7795db9e15054f42
Sawyer McLane <samclane@gmail.com>
2019-04-07 00:19:30 -0600

    Added speech bubbles

--- hello/views.py
+++ hello/views.py
@@ -1,10 +1,11 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from gettingstarted.settings import BASE_DIR
 import os
 import uuid
 
+from django.http import HttpResponse
+from django.shortcuts import render
 from gtts import gTTS
+
+from gettingstarted.settings import BASE_DIR
 
 if os.name != 'nt':
     BASE_DIR = "/tmp"
@@ -16,14 +17,15 @@ else:
 def index(request):
     if request.method == 'POST':
         uid = uuid.uuid4()
+        filename = r"file_{}.mp3".format(uid)
         phrase = request.POST.get('textfield', None)
         try:
             tts = gTTS(phrase, lang='en')
-            with open(os.path.join(BASE_DIR, r"file_{}.mp3".format(uid)), 'wb') as f:
+            with open(os.path.join(BASE_DIR, filename), 'wb') as f:
                 tts.write_to_fp(f)
-            return render(request, "index.html", {"link": os.path.join(BASE_DIR, r"file_{}.mp3".format(uid))})
+            return render(request, "index.html", {"link": os.path.join(BASE_DIR, filename),
+                                                  "phrase": phrase})
         except Exception as exc:
             return HttpResponse(f"Exception thrown during speak attempt: {exc}\n")
     else:
         return render(request, "index.html")
-
